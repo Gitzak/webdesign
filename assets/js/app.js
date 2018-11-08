475bbf03177fa587ce3de539566d13e7e36f20b7
zakaria belrhali <zakaria.belrhali@gmail.com>
2018-11-08 08:55:56 +0000

    mise à jour isotope js ça marche bien

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,3 +39,13 @@ $('.js-scroll').on('click', function(event) {
     scrollTop: $(target).offset().top
   }, 800);
 });
+
+// init Isotope
+var $grid = $('.grid').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
